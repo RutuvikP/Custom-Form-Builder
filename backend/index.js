fa5519a5dd498379980b0eabc5f43b1ec76559e7
Rutuvik Patil <you@example.com>
2023-07-29 17:33:54 +0530

    Response Route and Response Model

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,14 @@ const express=require('express');
 const cors=require('cors');
 const { connection } = require('./configs/db');
 const { formRoutes } = require('./routes/Form.routes');
+const { responseRouter } = require('./routes/Response.routes');
 const app=express();
 
 app.use(cors())
 app.use(express.json())
 
 app.use('/forms',formRoutes)
+app.use('/response',responseRouter)
 
 app.get("/",(req,res)=>{
     res.send("Welcome to Backend!!")
